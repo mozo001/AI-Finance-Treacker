461892d0d26b2fe8cc7e2e8d673e9db6548be5e4
mafiz <mafizchowdhury006@gmail.com>
2026-05-22 21:03:40 +0600

    fix: database path resolving for serverless builds

--- Personal_Finance_Tracker/core/wsgi.py
+++ Personal_Finance_Tracker/core/wsgi.py
@@ -6,11 +6,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
-
 import os
-
+import sys
+from pathlib import Path
 from django.core.wsgi import get_wsgi_application
-from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(str(BASE_DIR))
 
 load_dotenv()
 
