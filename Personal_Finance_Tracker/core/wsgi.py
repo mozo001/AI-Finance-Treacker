7ab657b790ced68a96718f774c0cc5129f89f017
mafiz <mafizchowdhury006@gmail.com>
2026-05-23 21:23:16 +0600

    fix

--- Personal_Finance_Tracker/core/wsgi.py
+++ Personal_Finance_Tracker/core/wsgi.py
@@ -12,11 +12,7 @@ from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-<<<<<<< HEAD
 sys.path.append(str(BASE_DIR))
-=======
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
->>>>>>> 60e1e653dcc77deac03169f5a5537d6a03444de4
 
 load_dotenv()
 
