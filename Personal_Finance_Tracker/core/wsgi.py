4633ba28e6d221cf6a6ae86a1250f86cdf170cdf
Mafiz Uddin Chowdhury <mafizchowdhury006@gmail.com>
2026-05-23 22:42:34 +0600

    Update wsgi.py

--- Personal_Finance_Tracker/core/wsgi.py
+++ Personal_Finance_Tracker/core/wsgi.py
@@ -20,7 +20,7 @@ load_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 DATABASES = {
-    'default': dj_database_url.parse('postgresql://postgres:SgwzrVnTLogbCuk1@db.xciicjvkqbywubxndtvt.supabase.co:5432/postgres')
+    'default': dj_database_url.parse('postgres://postgres:SgwzrVnTLogbCuk1@db.xciicjvkqbywubxndtvt.supabase.co:5432/postgres')
 }
 application = get_wsgi_application()
 app = application
