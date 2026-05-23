24be7060661aca8de30914af9a4afbef825f9386
Mafiz Uddin Chowdhury <mafizchowdhury006@gmail.com>
2026-05-23 21:28:33 +0600

    Update wsgi.py

--- Personal_Finance_Tracker/core/wsgi.py
+++ Personal_Finance_Tracker/core/wsgi.py
@@ -10,7 +10,7 @@ import os
 import sys
 from pathlib import Path
 from django.core.wsgi import get_wsgi_application
-
+from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.append(str(BASE_DIR))
 
