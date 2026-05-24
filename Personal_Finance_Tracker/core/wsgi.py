2f7be87412fcd792fe29e69ad8661b2832f7764a
mafiz <mafizchowdhury006@gmail.com>
2026-05-24 10:24:06 +0600

    fix the sql

--- Personal_Finance_Tracker/core/wsgi.py
+++ Personal_Finance_Tracker/core/wsgi.py
@@ -1,26 +1,16 @@
-"""
-WSGI config for core project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
 import os
 import sys
 from pathlib import Path
 from django.core.wsgi import get_wsgi_application
-import dj_database_url
-from dotenv import load_dotenv
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.append(str(BASE_DIR))
 
-load_dotenv()
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-DATABASES = {
-    'default': dj_database_url.parse('postgresql://postgres.xciicjvkqbywubxndtvt:SgwzrVnTLogbCuk1@aws-1-ap-northeast-1.pooler.supabase.com:6543/postgres')
-}
+
 application = get_wsgi_application()
 app = application
